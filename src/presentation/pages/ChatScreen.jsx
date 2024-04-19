96228bdb4f2adbf172fcc304fe5caccebfa45d6f
alexander67x <alexandernavarro6721@gmail.com>
2024-04-19 16:50:37 -0400

    cambiochatscreen

--- src/presentation/pages/ChatScreen.jsx
+++ src/presentation/pages/ChatScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Chat } from "../../infraestructure/api/chat";
 import App from '../App';
 import ForgotPassword from './ForgotPassword';
@@ -14,6 +14,9 @@ function ChatScreen() {
     const [showPopup, setShowPopup] = useState(false);
     const [isTyping, setIsTyping] = useState(false);
     const [isListening, setIsListening] = useState(false);
+    const recognitionRef = useRef(null);
+    const inactivityTimer = useRef(null);
+    const [helpMessageSent, setHelpMessageSent] = useState(false);  // Estado para controlar si se ha enviado el mensaje de ayuda
 
     useEffect(() => {
         const welcomeMessage = "Hola soy AVU, bienvenido. ¿En qué puedo ayudarte?";
@@ -33,38 +36,59 @@ function ChatScreen() {
         const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
         const recognition = new SpeechRecognition();
         recognition.lang = 'es-ES';
+        recognition.continuous = false;
+        recognitionRef.current = recognition;
+
         recognition.onstart = () => {
             setIsListening(true);
         };
 
+        recognition.onerror = (event) => {
+            console.error("Error en el reconocimiento de voz:", event.error);
+        };
+
         recognition.onresult = (event) => {
             const transcript = event.results[0][0].transcript;
-            setNewMessage(transcript);
+            setNewMessage(prev => prev + (prev.length > 0 ? " " : "") + transcript);
         };
 
         recognition.onend = () => {
             setIsListening(false);
         };
 
-        const startListening = () => {
-            recognition.start();
-        };
-
-        window.startListening = startListening;
-
         return () => {
             recognition.stop();
-            window.startListening = null;
+            recognitionRef.current = null;
         };
     }, []);
+
+    useEffect(() => {
+        if (!helpMessageSent) {
+            clearTimeout(inactivityTimer.current);
+            inactivityTimer.current = setTimeout(() => {
+                setMessages(messages => [...messages, { from: 'assistant', text: "¿Necesitas ayuda en algo más?" }]);
+                setHelpMessageSent(true);
+            }, 5000);
+        }
+
+        return () => clearTimeout(inactivityTimer.current);
+    }, [messages, helpMessageSent]);
+
+    const toggleListening = () => {
+        if (isListening) {
+            recognitionRef.current.stop();
+        } else {
+            recognitionRef.current.start();
+        }
+    };
 
     const speakText = () => {
         const lastAssistantMessage = messages.reverse().find(m => m.from === 'assistant');
         if (lastAssistantMessage) {
             const speech = new SpeechSynthesisUtterance(lastAssistantMessage.text);
             speech.lang = 'es-ES';
-            speech.voice = speechSynthesis.getVoices()[7];//cambio de voz 7 , 2
-            speech.rate = 1.2//velocidad de habala
+            speech.voice = window.speechSynthesis.getVoices().find(v => v.lang === 'es-ES');
+            speech.rate = 1.2;
             window.speechSynthesis.speak(speech);
         }
     };
@@ -73,27 +97,12 @@ function ChatScreen() {
         if (newMessage.trim()) {
             setMessages(messages => [...messages, { from: 'user', text: newMessage }]);
             setIsTyping(true);
-
-            // Animación al enviar mensaje
-            setTimeout(() => setIsTyping(false), 2000);
+            setHelpMessageSent(false);  // Reset this flag so help message can be sent again if necessary
 
             Chat.answerQuestions({ question: newMessage }).then(res => {
                 setIsTyping(false);
                 let fullResponse = res.result;
-                let i = 0;
-                const typeWriter = () => {
-                    if (i < fullResponse.length) {
-                        const partialResponse = fullResponse.slice(0, i + 1);
-                        if (i === 0) {
-                            setMessages(messages => [...messages, { from: 'assistant', text: partialResponse }]);
-                        } else {
-                            setMessages(messages => [...messages.slice(0, -1), { from: 'assistant', text: partialResponse }]);
-                        }
-                        i++;
-                        setTimeout(typeWriter, 20);
-                    }
-                };
-                typeWriter();
+                setMessages(messages => [...messages, { from: 'assistant', text: fullResponse }]);
             });
 
             setNewMessage('');
@@ -102,6 +111,30 @@ function ChatScreen() {
 
     const handleNewMessageChange = (e) => {
         setNewMessage(e.target.value);
+    };
+
+    const renderMessage = (message) => {
+        const urlRegex = /(https?:\/\/[^\s]+)/g;
+        const phoneRegex = /(\b\d{8}\b)/g; 
+    
+        return message.text.split(/(https?:\/\/[^\s]+|\b\d{8}\b)/g).map((part, index) => {
+            if (urlRegex.test(part)) {
+                return (
+                    <a key={index} href={part} target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">
+                        {part}
+                    </a>
+                );
+            } else if (phoneRegex.test(part)) {
+                const whatsappUrl = `https://wa.me/591${part}`; 
+                return (
+                    <a key={index} href={whatsappUrl} target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">
+                        {part}
+                    </a>
+                );
+            } else {
+                return <span key={index}>{part}</span>;
+            }
+        });
     };
 
     const handleAssistantClick = () => {
@@ -130,19 +163,19 @@ function ChatScreen() {
                 <div className="chat-header bg-neutro-primary p-4 rounded-t-lg flex justify-between items-center">
                     <h1 className="text-2xl text-white font-bold">Chat con A.V.U</h1>
                     <button onClick={speakText} className="bg-neutro-primary text-white py-2 px-4 rounded-lg hover:animate-pulse border border-gray-300">
-                        leer Respuesta
+                        Leer Respuesta
                     </button>
                 </div>
                 <div className="chat-messages p-4 h-96 overflow-y-auto bg-white rounded-b-lg">
                     {messages.map((message, index) => (
-                        <div key={index} className={`whitespace-pre-line ${message.from === 'user' ? 'bg-neutro-tertiary' : 'bg-gray-400'} my-2 p-2 rounded-lg text-white animate__animated animate__fadeInUp`}>
-                            {message.text}
+                        <div key={index} className={`whitespace-pre-line ${message.from === 'user' ? 'bg-neutro-tertiary' : 'bg-gray-400'} my-2 p-2 rounded-lg text-white`}>
+                            {renderMessage(message)}
                         </div>
                     ))}
-                    {isTyping && <div className="message bg-gray-400 my-2 p-2 rounded-lg text-white animate__animated animate__fadeInUp">Cargando...</div>}
+                    {isTyping && <div className="message bg-gray-400 my-2 p-2 rounded-lg text-white">Cargando...</div>}
                 </div>
                 <div className="chat-input p-4 flex items-center">
-                    <button onClick={() => window.startListening()} className={`bg-neutro-primary text-white py-2 px-4 rounded-lg mr-2 ${isListening ? 'bg-red-500' : ''} hover:animate-pulse`}>
+                    <button onClick={toggleListening} className={`bg-neutro-primary text-white py-2 px-4 rounded-lg mr-2 ${isListening ? 'bg-red-500' : ''}`}>
                         {isListening ? '🛑' : '🎤'}
                     </button>
                     <input
