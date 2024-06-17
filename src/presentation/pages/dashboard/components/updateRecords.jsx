2e22088579fe39037e53131eb16262a275b93976
Luis <luispadillarandia@gmail.com>
2024-06-17 12:26:17 -0700

    Updated related to login and add some register view card for mobile devices

--- src/presentation/pages/dashboard/components/updateRecords.jsx
+++ src/presentation/pages/dashboard/components/updateRecords.jsx
@@ -151,10 +151,10 @@ const UpdateRecord = ({ item, onUpdate, onClose }) => {
             isOpen={true}
             onRequestClose={onClose}
             contentLabel="Actualizar Registro"
-            className="fixed w-full inset-0 flex items-center justify-center"
+            className="fixed inset-0 flex items-center justify-center z-50"
             overlayClassName="fixed inset-0 bg-black bg-opacity-50  z-50"
         >
-            <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-lg">
+             <div className="bg-white p-6 rounded-lg shadow-lg w-3/4 max-w-lg sm:w-3/5">
                 <div className="flex justify-between items-center mb-4">
                     <h2 className="text-lg font-semibold">Actualizar Registro</h2>
                     <button
--- src/presentation/pages/home/views/login.jsx
+++ src/presentation/pages/home/views/login.jsx
@@ -34,40 +34,85 @@ const Login = () => {
     if (needValidationEmail) return <NeedVerfication email={user.email} />
 
     return (
-        <div className="flex-grow flex">
-            <header className="bg-neutro-tertiary p-7 text-center text-white fixed top-0 w-full z-10">
-            </header>
-            <div className="w-[55%]" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-            </div>
-            <div className="flex-grow flex flex-col items-center justify-center bg-white p-8 text-black">
-                <div className="w-3/5">
-                    <h2 className="text-3xl mb-4">BIENVENIDO</h2>
-                    <p className="mb-8">ES UN GUSTO VOLVER A VERTE</p>
-                    <form onSubmit={handleLogin}>
-                        <div className="mb-4">
-                            <input type="email" id="email" required onChange={(e) => {
-                                setUser({ ...user, email: e.target.value })
-                            }} placeholder="Correo Electrónico" className="w-full p-2 rounded-md border-2 border-neutro-secondary focus:border-neutro-tertiary outline-none" />
-                        </div>
-                        <div className="mb-2">
-                            <input type="password" required onChange={(e) => {
-                                setUser({ ...user, password: e.target.value })
-                            }} placeholder="Contraseña" className="w-full p-2 rounded-md border-2 border-neutro-secondary focus:border-neutro-tertiary outline-none" />
-                        </div>
-                        <div className="text-right">
-                            <Link to="/forgot-password" className=" text-[#522B46] cursor-pointer mb-4 block">
-                                Olvidé mi contraseña
-                            </Link>
-                        </div>
-                        <button className="bg-neutro-tertiary w-full p-3 rounded-md hover:bg-[#A7A9AC] transition duration-300 text-white" >INICIAR SESION</button>
-                        <div className="flex gap-1 mt-4">
-                            <p>Aun no tienes una cuenta?</p>
-                            <Link to="/signup" className="text-neutro-tertiary">Registrate</Link>
-                        </div>
-                    </form>
+        <>
+            <div className="hidden lg:flex flex-grow">
+                <header className="bg-neutro-tertiary p-7 text-center text-white fixed top-0 w-full z-10">
+                </header>
+                <div className="w-[55%]" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                </div>
+                <div className="flex-grow flex flex-col items-center justify-center bg-white p-8 text-black">
+                    <div className="w-3/5">
+                        <h2 className="text-3xl mb-4">BIENVENIDO</h2>
+                        <p className="mb-8">ES UN GUSTO VOLVER A VERTE</p>
+                        <form onSubmit={handleLogin}>
+                            <div className="mb-4">
+                                <input type="email" id="email" required onChange={(e) => {
+                                    setUser({ ...user, email: e.target.value })
+                                }} placeholder="Correo Electrónico" className="w-full p-2 rounded-md border-2 border-neutro-secondary focus:border-neutro-tertiary outline-none" />
+                            </div>
+                            <div className="mb-2">
+                                <input type="password" required onChange={(e) => {
+                                    setUser({ ...user, password: e.target.value })
+                                }} placeholder="Contraseña" className="w-full p-2 rounded-md border-2 border-neutro-secondary focus:border-neutro-tertiary outline-none" />
+                            </div>
+                            <div className="text-right">
+                                <Link to="/forgot-password" className=" text-[#522B46] cursor-pointer mb-4 block">
+                                    Olvidé mi contraseña
+                                </Link>
+                            </div>
+                            <button className="bg-neutro-tertiary w-full p-3 rounded-md hover:bg-[#A7A9AC] transition duration-300 text-white" >INICIAR SESION</button>
+                            <div className="flex gap-1 mt-4">
+                                <p>Aun no tienes una cuenta?</p>
+                                <Link to="/signup" className="text-neutro-tertiary">Registrate</Link>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
+            <div className="lg:hidden min-h-screen flex flex-col">
+                <div className="flex-grow flex flex-col justify-center items-center relative overflow-hidden">
+                    <img className="absolute top-0 object-cover w-full h-full" src={backgroundImage} alt="Background" />
+                    <div className="bg-white bg-opacity-75 p-8 relative z-10 rounded-lg shadow-2xl w-11/12">
+                        <h2 className="text-4xl font-bold text-[#522B46] mb-6 text-center">BIENVENIDO</h2>
+                        <p className="text-center mb-8">ES UN GUSTO VOLVER A VERTE</p>
+                        <form onSubmit={handleLogin}>
+                            <div className="mb-4">
+                                <input 
+                                    type="email"
+                                    id="email"
+                                    required
+                                    onChange={(e) => setUser({ ...user, email: e.target.value })}
+                                    placeholder="Correo Electrónico"
+                                    className="w-full p-2 rounded-md"
+                                />
+                            </div>
+                            <div className="mb-2">
+                                <input
+                                    type="password"
+                                    required
+                                    onChange={(e) => setUser({ ...user, password: e.target.value })}
+                                    placeholder="Contraseña"
+                                    className="w-full p-2 rounded-md"
+                                />
+                            </div>
+                            <div className="text-right">
+                                <Link to="/forgot-password" className="text-[#522B46] cursor-pointer mb-4 block">
+                                    Olvidé mi contraseña
+                                </Link>
+                            </div>
+                            <button className="bg-[#522B46] text-white rounded-md p-2 w-full mt-4 hover:bg-[#A7A9AC] transition-colors duration-300">
+                                INICIAR SESION
+                            </button>
+                            <div className="flex gap-1 justify-center mt-4">
+                                <p>Aun no tienes una cuenta?</p>
+                                <Link to="/signup" className="text-[#522B46]">Registrate</Link>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
+
 export default Login;
